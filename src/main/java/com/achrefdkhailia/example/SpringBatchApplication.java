8577f332d8002e90a29e3121e4df2b8d73aa978e
dkhailiaAchref <achref.dkhailia@gmail.com>
2019-10-01 11:24:23 +0200

    clean up main application

--- src/main/java/com/achrefdkhailia/example/SpringBatchApplication.java
+++ src/main/java/com/achrefdkhailia/example/SpringBatchApplication.java
@@ -1,14 +1,10 @@
 package com.achrefdkhailia.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@ComponentScan
-@Configuration
-@PropertySource(value = {"classpath:application.properties"})
+@SpringBootApplication
 public class SpringBatchApplication {
 
 	public static void main(String[] args) {
